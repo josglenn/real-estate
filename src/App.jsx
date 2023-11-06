71a3e96ce5a9dc9914e707996d6ec913e7123b5b
jos glenn <josglenn123@gmail.com>
2023-11-06 17:32:46 +0800

    install react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-black">
+        <p className="text-white text-4xl font-montserrat">Hey</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
