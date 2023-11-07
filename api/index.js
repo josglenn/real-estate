d17a691128449401821df1ce995e2e5ea89440ed
jos glenn <josglenn123@gmail.com>
2023-11-07 10:45:30 +0800

    connect to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MOngo Conencted");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("app is running on prot 3000");
